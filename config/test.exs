9d1cc12f070e5b5026c4ae2a8dddcdb01679f522
madawei2699 <madawei2699@gmail.com>
2020-11-25 23:31:07 +0800

    fix github action test with postgres

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :invest_api, InvestApi.Repo,
   username: "postgres",
   password: "123456",
   database: "invest_api_dev#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
