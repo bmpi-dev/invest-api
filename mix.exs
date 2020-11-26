42c7a15b15985895a0a85d92fe957c8acb58869d
madawei2699 <madawei2699@gmail.com>
2020-11-26 08:34:57 +0800

    fix github action dialyzer error

--- mix.exs
+++ mix.exs
@@ -83,6 +83,7 @@ defmodule InvestApi.MixProject do
 
   defp dialyzer do
     [
+      plt_add_apps: [:ex_unit],
       plt_core_path: "priv/plts",
       plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
